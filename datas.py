7848da11b7037d6c1685d2d918412965c64a13e2
Roman4848 <xstar@list.ru>
2024-08-09 18:30:41 +0300

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,6 @@
+print(2.0)  # // - целочисленное деление , / - деление
+print(type(2.0))
+print("Helloy, word")   # string - строка
+print (type('Helloy word'))
+print('Helloy, '+'word!')                #concatenate
+print(type(7.8))
