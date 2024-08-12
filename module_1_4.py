d9607fc4420e72a804b972cbc38f5cba2f68cf5b
Roman4848 <xstar@list.ru>
2024-08-12 17:30:12 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,15 @@
+# my_string = input('введи строку:')
+# print("Длина введённого текста:", len(my_string))
+#print('НИЖНИЙ РЕГИСТР'. lower())
+#print ('Я, любля слушать музыку король и шут'.replace(__old:=' ',__new:=''))
+#my_string='верхний регистр'.upper()
+#print(my_string)
+#my_string='НИЖНИЙ РЕГИСТР'. lower()
+#print(my_string)
+my_string= 'Я, любля слушать музыку король и шут'
+print(my_string[0::35])
+my_string= 'Я, любля слушать музыку король и шут'
+print(my_string[0])
+my_string= 'Я, любля слушать музыку король и шут'
+print(my_string[-1])
+
