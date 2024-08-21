db153b577d65f0de6bee8db3cad0180682e99004
Roman4848 <xstar@list.ru>
2024-08-21 11:41:00 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,17 @@
+string = ''
+for k in range(3, 21):
+    string = ''
+    for i in range(1, k):
+        for j in range(i + 1, k):
+            if k % (i + j) == 0:
+                string += str(i) + str(j)
+    print(k, '-', string)
+
+
+for k in range(3, 21):
+    string = ''
+    for i in range(1, k):
+        for j in range(i + 1, k):
+            if k % (i + j) == 0:
+                string += str(i) + '+' + str(j) + ' '
+    print(k, '-', string)
