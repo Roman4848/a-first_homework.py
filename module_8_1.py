00baac78ffde90029d1405998843555729040080
Roman4848 <xstar@list.ru>
2024-10-12 16:00:59 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,18 @@
+def add_everything_up(a, b):
+    try:
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return '{:.3f}{}'.format(a + b, '' if isinstance(b, int) else b)
+        elif isinstance(a, str) and isinstance(b, str):
+            return a + b
+        elif isinstance(a, (int, float)) and isinstance(b, str):
+            return '{:.3f}{}'.format(a, b)
+        elif isinstance(a, str) and isinstance(b, (int, float)):
+            return '{}{}'.format(a, '{:.0f}'.format(b))
+        else:
+            raise TypeError("Both arguments must be either numbers or strings")
+    except TypeError as e:
+        return str(e)
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
