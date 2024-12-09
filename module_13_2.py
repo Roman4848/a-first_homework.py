2fa941843f89862a3a8960ed24c9968004e74705
Roman4848 <xstar@list.ru>
2024-12-10 00:54:36 +0300

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -8,22 +8,13 @@ bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
 
-@dp.message_handler(text=['Urban', 'ff'])
-async def urban_message(message: types.Message):
-    print("Urban message")
-    await message.reply("Вы отправили специальное сообщение: Urban или ff!")
-
-
 @dp.message_handler(commands=['start'])
-async def start_message(message: types.Message):
-    print("Start message")
-    await message.reply("Привет! Я бот, помогающий твоему здоровью.")
-
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message_handler()
-async def all_message(message: types.Message):
-    print("Мы получили сообщение")
-    await message.reply("Спасибо за ваше сообщение! Напишите /start, чтобы начать.")
+async def all_massages(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates = True)
